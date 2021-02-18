dd38a038d50c507e47e5902bb1a7c5ac70b39d56
Zain Raza <zainoakgrove@gmail.com>
2021-02-18 10:56:14 -0600

    Got API key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -27,12 +27,7 @@ struct ContentView: View {
     }
     
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=1f4c88104emshef1c1b78b1ea540p19cfeajsn3ef97fdc626c"
     }
 }
 
